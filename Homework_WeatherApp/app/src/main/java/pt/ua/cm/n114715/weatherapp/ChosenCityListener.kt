c6098e491ce3bc63c61d37c19e538b59ac8522e6
Decathlannix <73560652+Decathlannix@users.noreply.github.com>
2022-12-05 21:25:39 +0000

    Delete Homework_WeatherApp directory

--- Homework_WeatherApp/app/src/main/java/pt/ua/cm/n114715/weatherapp/ChosenCityListener.kt
+++
@@ -1,7 +0,0 @@
-package pt.ua.cm.n114715.weatherapp
-
-// Interface used by the activity to implement the method to create and
-// send data to a forecast fragment, and called by the main fragment, CitiesFragment
-interface ChosenCityListener {
-    fun onCityChosen (position: Int)
-}
