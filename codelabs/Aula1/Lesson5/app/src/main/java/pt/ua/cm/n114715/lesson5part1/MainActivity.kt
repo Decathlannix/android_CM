aaedace9d070af2f2f3b0e11b566709c8264eeba
Decathlannix <73560652+Decathlannix@users.noreply.github.com>
2022-12-06 17:31:01 +0000

    Add files via upload

---
+++ codelabs/Aula1/Lesson5/app/src/main/java/pt/ua/cm/n114715/lesson5part1/MainActivity.kt
@@ -0,0 +1,11 @@
+package pt.ua.cm.n114715.lesson5part1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
